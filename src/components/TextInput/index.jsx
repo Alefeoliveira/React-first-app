fdb57178e2be2baf5251612469c625d535ce9d0d
Alefe Oliveira <alefe.oliveira3@hotmail.com>
2021-06-02 16:40:36 -0300

    initial

---
+++ src/components/TextInput/index.jsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+export const TextInput = ({searchValue, handleChange}) =>(
+   
+  <input 
+          className="text-input"
+          onChange={handleChange}
+          type="search" 
+          value={searchValue}
+          placeholder="type your text"
+        />
+)
